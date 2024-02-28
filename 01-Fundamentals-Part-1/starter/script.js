5ba90179f82afe4503eb24fd89d2a86f1ca4bd22
Ahmed Elbagory <143671343+aelbagory2023@users.noreply.github.com>
2024-02-27 20:51:32 -0500

    first code challange complete

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,27 @@
-let js= "amazing"
-if(js=== "amazing") alert("js is fun")
+let js = "amazing";
 
-console.log(12+234+32-23)
+console.log(12 + 234 + 32 - 23);
+const now = 2038;
+const ageJonas = now - 1991;
+const ageSarah = now - 2014;
+console.log(ageJonas, ageSarah);
+
+const firstName = "Jonas";
+const lastName = "Shmidt";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+console.log(x);
+
+const weightMark = 78;
+const heightMark = 1.69;
+const bmiMark = weightMark / (heightMark * heightMark);
+
+const weightJohn = 92;
+const heightJohn = 1.95;
+const bmiJohn = weightJohn / (heightJohn * heightJohn)
+
+let markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI)
