cb5d3f82e417271731c36177b1c69b9246dc83ef
Ahmed Elbagory <143671343+aelbagory2023@users.noreply.github.com>
2024-02-29 20:03:56 -0500

    code challenge 4 complete

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -54,3 +54,15 @@ if (scoreDolphins === scoreKoalas) {
 } else {
   console.log("Koalas win the trophy");
 }
+
+const bill = 275;
+const tip = bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  "The bill was " +
+    bill +
+    ", the tip was " +
+    tip +
+    ", and the total value " +
+    (bill + tip) +
+    "."
+);
