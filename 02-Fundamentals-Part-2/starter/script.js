a775796ae7abcaae8e01b8f1cd9514c119981b87
Ahmed Elbagory <143671343+aelbagory2023@users.noreply.github.com>
2024-03-02 01:52:04 -0500

    yearsUntilRetirement Function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,11 +6,11 @@ logger();
 logger();
 logger();
 
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
+// //function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
 
 const appleJuice = fruitProcessor(4, 0);
 console.log(appleJuice);
@@ -35,3 +35,39 @@ console.log(age2);
 
 console.log(age1, age2);
 
+const calcAge3 = (birthyear) => 2024 - birthyear;
+
+const age3 = calcAge3(1997);
+console.log(age3);
+
+const yearsUntilRetirement = (birthyear, firstName) => {
+  const age = calcAge3(birthyear);
+  const retirement = 65 - age;
+  const retirementYear = 2024 + retirement;
+  if (retirement == 0) {
+    return `${firstName} retires this year in ${retirementYear}.`;
+  } else if (retirement < 0) {
+    let yearsRetired = retirement * -1;
+    return `${firstName} has been retired for ${yearsRetired} years since ${retirementYear}.`;
+  } else {
+    return `${firstName} retires in ${retirement} years in ${retirementYear}.`;
+  }
+};
+
+console.log(yearsUntilRetirement(1996, "Ahmed"));
+
+console.log(yearsUntilRetirement(1959, "Mike"));
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  console.log(apples, oranges);
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
